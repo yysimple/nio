c58eebc2a7dd4791458ecee93081ef4aeaa19cd7
yysimple <5325809+yysimple@user.noreply.gitee.com>
2021-03-28 19:37:51 +0800

    [新增]：阻塞io和非阻塞io的测试

---
+++ src/main/java/com/wcx/nio/pipe/PipeTest.java
@@ -0,0 +1,40 @@
+package com.wcx.nio.pipe;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.Pipe;
+
+/**
+ * 项目: nio
+ * <p>
+ * 功能描述:
+ *
+ * @author: WuChengXing
+ * @create: 2021-03-28 17:57
+ **/
+public class PipeTest {
+    @Test
+    public void test1() throws IOException {
+        //1. 获取管道
+        Pipe pipe = Pipe.open();
+
+        //2. 将缓冲区中的数据写入管道
+        ByteBuffer buf = ByteBuffer.allocate(1024);
+
+        Pipe.SinkChannel sinkChannel = pipe.sink();
+        buf.put("通过单向管道发送数据".getBytes());
+        buf.flip();
+        sinkChannel.write(buf);
+
+        //3. 读取缓冲区中的数据
+        Pipe.SourceChannel sourceChannel = pipe.source();
+        buf.flip();
+        int len = sourceChannel.read(buf);
+        System.out.println(new String(buf.array(), 0, len));
+
+        sourceChannel.close();
+        sinkChannel.close();
+    }
+}
